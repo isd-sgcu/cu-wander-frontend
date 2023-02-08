3c33bbca979e3bed59b57150c8aeaa64b23cf274
Ebonian <tanadon.santisan@gmail.com>
2023-02-08 08:55:05 +0700

    build: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        green: {
+          50: "#EEFBF6",
+          300: "#AEECD3",
+          500: "#138F71",
+          700: "#0E5747",
+        },
+      },
+      fontFamily: {
+        noto: ["Noto Sans Thai", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
