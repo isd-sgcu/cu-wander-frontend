172f21d2059c683cc27f986948fb8ddb9847402e
Tanadon Santisan <tanadon.santisan@gmail.com>
2023-03-24 23:20:08 +0700

    feat: implement search function

--- src/pages/Coupon.tsx
+++ src/pages/Coupon.tsx
@@ -4,6 +4,7 @@ import CouponItem from "../components/coupon/CouponItem";
 import CouponModal from "../components/coupon/CouponModal";
 import Header from "../components/Header";
 import { showTabBar } from "../utils/tab";
+import mockCouponData from "../data/mockCouponData.json";
 
 export const CouponContext = createContext({
   showModal: false,
@@ -17,9 +18,17 @@ const Coupon: React.FC = () => {
 
   const [showModal, setShowModal] = useState<boolean>(false);
 
+  const [searchPhrase, setSearchPhrase] = useState<string>("");
+
+  const mockData = mockCouponData;
+
   return (
     <IonPage>
-      <Header title="คูปอง" />
+      <Header
+        title="คูปอง"
+        searchPhrase={searchPhrase}
+        setSearchPhrase={setSearchPhrase}
+      />
       <IonContent fullscreen>
         <CouponContext.Provider value={{ showModal, setShowModal }}>
           {/* modal */}
@@ -27,18 +36,25 @@ const Coupon: React.FC = () => {
 
           {/* page content */}
           <div className="flex flex-col items-center bg-white font-noto pt-12 pb-6 space-y-2">
-            {Array(20)
-              .fill("")
-              .map((x, idx) => {
-                return (
-                  <CouponItem
-                    key={idx}
-                    name="THB 50 e-Coupon"
-                    merchant="BBQ Plaza"
-                    steps={12500}
-                  />
-                );
-              })}
+            {mockData.map((coupon, idx) => {
+              if (searchPhrase !== "") {
+                if (
+                  !coupon.name.toLowerCase().includes(searchPhrase) &&
+                  !coupon.merchant.toLowerCase().includes(searchPhrase)
+                ) {
+                  return null;
+                }
+              }
+
+              return (
+                <CouponItem
+                  key={idx}
+                  name={coupon.name}
+                  merchant={coupon.merchant}
+                  steps={coupon.steps}
+                />
+              );
+            })}
           </div>
         </CouponContext.Provider>
       </IonContent>
