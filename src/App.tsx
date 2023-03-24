761574abae59574add7fa0fec26b96d56db8d0b7
Tanadon Santisan <tanadon.santisan@gmail.com>
2023-03-25 04:32:59 +0700

    feat: modal context

--- src/App.tsx
+++ src/App.tsx
@@ -43,77 +43,83 @@ import "./theme/tailwind.css";
 import "./theme/variables.css";
 import Profile from "./pages/Profile";
 import CouponContext from "./contexts/CouponContext";
+import ModalContext from "./contexts/ModalContext";
 
 setupIonicReact();
 
 const App: React.FC = () => (
   <IonApp>
-    {/* Provide Context to the app */}
+    {/* provide Context to the app */}
     <CouponContext>
-      <IonReactRouter>
-        <IonTabs>
-          {/* page's router */}
-          <IonRouterOutlet>
-            {/* onboarding */}
-            <Route exact path="/onboarding">
-              <Onboarding />
-            </Route>
-            <Route exact path="/signup">
-              <Signup />
-            </Route>
-            <Route exact path="/signin">
-              <Signin />
-            </Route>
+      <ModalContext>
+        {/* router */}
+        <IonReactRouter>
+          <IonTabs>
+            {/* page's router */}
+            <IonRouterOutlet>
+              {/* onboarding */}
+              <Route exact path="/onboarding">
+                <Onboarding />
+              </Route>
+              <Route exact path="/signup">
+                <Signup />
+              </Route>
+              <Route exact path="/signin">
+                <Signin />
+              </Route>
 
-            {/* app */}
-            <Route exact path="/step">
-              <Step />
-            </Route>
-            <Route path="/coupon">
-              <Coupon />
-            </Route>
-            <Route exact path="/leaderboard">
-              <Leaderboard />
-            </Route>
-            <Route path="/profile">
-              <Profile />
-            </Route>
+              {/* app */}
+              <Route exact path="/step">
+                <Step />
+              </Route>
+              <Route path="/coupon">
+                <Coupon />
+              </Route>
+              <Route exact path="/leaderboard">
+                <Leaderboard />
+              </Route>
+              <Route path="/profile"></Route>
 
-            {/* redirect */}
-            <Route exact path="/">
-              <Redirect to="/onboarding" />
-            </Route>
-          </IonRouterOutlet>
+              {/* redirect */}
+              <Route exact path="/">
+                <Redirect to="/onboarding" />
+              </Route>
+            </IonRouterOutlet>
 
-          {/* tab bar */}
-          <IonTabBar
-            slot="bottom"
-            id="app-tab-bar"
-            className="bg-green-50 h-[72px] font-noto"
-          >
-            <IonTabButton className="bg-green-50" tab="step" href="/step">
-              <img src="assets/icon/shoe.svg" alt="นับเก้า" />
-              <IonLabel className="text-black">นับก้าว</IonLabel>
-            </IonTabButton>
-            <IonTabButton className="bg-green-50" tab="coupon" href="/coupon">
-              <img src="assets/icon/ticket.svg" alt="คูปอง" />
-              <IonLabel className="text-black">คูปอง</IonLabel>
-            </IonTabButton>
-            <IonTabButton
-              className="bg-green-50"
-              tab="leaderboard"
-              href="/leaderboard"
+            {/* tab bar */}
+            <IonTabBar
+              slot="bottom"
+              id="app-tab-bar"
+              className="bg-green-50 h-[72px] font-noto"
             >
-              <img src="assets/icon/star.svg" alt="ลีดเดอร์บอร์ด" />
-              <IonLabel className="text-black">ลีดเดอร์บอร์ด</IonLabel>
-            </IonTabButton>
-            <IonTabButton className="bg-green-50" tab="profile" href="/Profile">
-              <img src="assets/icon/user.svg" alt="ผู้ใช้งาน" />
-              <IonLabel className="text-black">ผู้ใช้งาน</IonLabel>
-            </IonTabButton>
-          </IonTabBar>
-        </IonTabs>
-      </IonReactRouter>
+              <IonTabButton className="bg-green-50" tab="step" href="/step">
+                <img src="assets/icon/shoe.svg" alt="นับเก้า" />
+                <IonLabel className="text-black">นับก้าว</IonLabel>
+              </IonTabButton>
+              <IonTabButton className="bg-green-50" tab="coupon" href="/coupon">
+                <img src="assets/icon/ticket.svg" alt="คูปอง" />
+                <IonLabel className="text-black">คูปอง</IonLabel>
+              </IonTabButton>
+              <IonTabButton
+                className="bg-green-50"
+                tab="leaderboard"
+                href="/leaderboard"
+              >
+                <img src="assets/icon/star.svg" alt="ลีดเดอร์บอร์ด" />
+                <IonLabel className="text-black">ลีดเดอร์บอร์ด</IonLabel>
+              </IonTabButton>
+              <IonTabButton
+                className="bg-green-50"
+                tab="profile"
+                href="/Profile"
+              >
+                <img src="assets/icon/user.svg" alt="ผู้ใช้งาน" />
+                <IonLabel className="text-black">ผู้ใช้งาน</IonLabel>
+              </IonTabButton>
+            </IonTabBar>
+          </IonTabs>
+        </IonReactRouter>
+      </ModalContext>
     </CouponContext>
   </IonApp>
 );
---
+++ src/components/Modal.tsx
@@ -0,0 +1,91 @@
+import { useContext } from "react";
+import { ModalState } from "../contexts/ModalContext";
+
+const Modal: React.FC = () => {
+  const {
+    showModal,
+    setPromptModal,
+    modalContent: content,
+  } = useContext(ModalState);
+
+  return (
+    <>
+      {/* background */}
+      <div
+        className={`bg-black fixed top-0 left-0 right-0 bottom-0 duration-300 z-40 flex ${
+          showModal ? "bg-opacity-40" : "bg-opacity-0 pointer-events-none"
+        }`}
+        onClick={() => setPromptModal(false)}
+      ></div>
+
+      <div
+        className={`fixed flex justify-center items-center left-7 right-7 top-0 bottom-0 pointer-events-none z-50 duration-200 ${
+          showModal ? "opacity-100" : "opacity-0 pointer-events-none"
+        }`}
+      >
+        {/* modal body */}
+        <div className="bg-white w-full rounded-2xl text-black overflow-hidden pointer-events-auto text-center font-noto relative flex flex-col justify-between">
+          {content.type === "default" && (
+            <div
+              onClick={() => setPromptModal(false)}
+              className="absolute top-0 right-0 p-5"
+            >
+              <img src="assets/icon/close.svg" alt="close" />
+            </div>
+          )}
+          <div
+            className={`px-5 w-full ${
+              content.type === "default" ? "py-8" : "py-6"
+            }`}
+          >
+            <h2 className="font-bold text-lg mb-2">{content.title}</h2>
+            <p>{content.subtitle}</p>
+          </div>
+          {content.body && <div className="pb-10">{content.body}</div>}
+          {content.hasOwnProperty("choices") && (
+            <div
+              className={`w-full flex items-center justify-center ${
+                content.type === "single" ? "h-10 mb-4" : "h-12"
+              }`}
+            >
+              {content.choices!.map((choice, idx) => {
+                return (
+                  <>
+                    <div
+                      key={idx}
+                      className={`h-full ${
+                        content.type === "multiple"
+                          ? "w-full border-t-[2px]"
+                          : "bg-green-500 rounded-lg px-6"
+                      } border-gray-200 grid place-content-center ${
+                        choice.primary
+                          ? content.type === "single"
+                            ? "text-white font-semibold"
+                            : "text-green-500 font-semibold"
+                          : content.type === "single"
+                          ? "text-white font-semibold"
+                          : "text-gray-500 font-medium"
+                      }`}
+                      onClick={() => choice.action()}
+                    >
+                      {choice.title}
+                    </div>
+                    {content.type !== "single" && idx % 2 === 0 ? (
+                      <div
+                        key={idx + 1}
+                        className="w-[2.5px] h-full bg-gray-200"
+                      ></div>
+                    ) : null}
+                    {}
+                  </>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
