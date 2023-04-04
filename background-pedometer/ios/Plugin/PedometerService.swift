37f3cc5f230133d2abba32f9083258fba2aa1475
betich <betich365@gmail.com>
2023-04-04 14:14:24 +0700

    chore: build ios

--- background-pedometer/ios/Plugin/PedometerService.swift
+++ background-pedometer/ios/Plugin/PedometerService.swift
@@ -3,6 +3,7 @@ import Foundation
 @objc public class PedometerService: NSObject {
     @objc public func echo(_ value: String) -> String {
         print(value)
+        print("Hello Test")
         return value
     }
 }
