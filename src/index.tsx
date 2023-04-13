49c0389429c392965123efa64f63d596c858f22f
tontan2545 <tontan2545@gmail.com>
2023-04-14 03:44:22 +0700

    feat: test sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
+import * as Sentry from "@sentry/capacitor";
 
-const container = document.getElementById('root');
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN,
+});
+
+Sentry.captureException("Test Captured Exception");
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
