703d8b5a2925fb4bb75db52d50f74cb72f26576c
tontan2545 <tontan2545@gmail.com>
2023-04-14 17:44:10 +0700

    feat: support socket refresh and move socket to connect on react

--- background-pedometer/ios/Plugin/PedometerServicePlugin.swift
+++ background-pedometer/ios/Plugin/PedometerServicePlugin.swift
@@ -6,7 +6,6 @@ import Capacitor
 public class PedometerServicePlugin: CAPPlugin {
     
     var service: PedometerService?
-    var webSocket: WebSocketConnection?
     
     @objc func requestPermission(_ call: CAPPluginCall) {
         var data = Dictionary<String, Any>()
@@ -23,13 +22,6 @@ public class PedometerServicePlugin: CAPPlugin {
             return
         }
         
-        let token = call.getString("token")
-        let wsAddress = call.getString("wsAddress")
-      
-        if token != nil && wsAddress != nil {
-            self.webSocket = WebSocketConnection(authToken: String(describing: token!), wsAddress: String(describing: wsAddress!))
-        }
-        
         service = PedometerService()
         service?.plugin = self
         
@@ -44,17 +36,6 @@ public class PedometerServicePlugin: CAPPlugin {
     
     func fireSteps(_ steps: Int) {
         let data: [String: Any] = ["steps": steps]
-        let stepJson: [String: Any] = ["step": steps]
-        if(self.webSocket !== nil) {
-            do {
-                let jsonData = try JSONSerialization.data(withJSONObject: stepJson, options: .prettyPrinted)
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-                    self.webSocket?.send(data: jsonString)
-                }
-            } catch {
-                print("Error serializing data to JSON: \(error)")
-            }
-        }
         
         notifyListeners("steps", data: data)
     }
