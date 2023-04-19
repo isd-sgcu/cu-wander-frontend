e2c65ab3b96fd10e6002d734052e6ce5f077491c
betich <betich365@gmail.com>
2023-04-20 02:37:13 +0700

    fix: redirect to /Onboarding if user is not logged in

--- src/lib/auth/guard/PrivateRoute.tsx
+++ src/lib/auth/guard/PrivateRoute.tsx
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
 import { useAuth } from "../../../contexts/AuthContext";
 
 const PrivateRoute = ({ component: Component, ...rest }: any) => {
   const { isLoggedIn } = useAuth();
   const [loading, setLoading] = useState(true);
   const [loggedIn, setLoggedIn] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
     const checkLoggedIn = async () => {
       const loggedIn = await isLoggedIn();
       setLoggedIn(loggedIn);
       setLoading(false);
+
+      // if not logged in, redrect to /Onboarding
+      if (!loggedIn) {
+        history.push("/Onboarding");
+      }
     };
 
     checkLoggedIn();
@@ -23,12 +29,7 @@ const PrivateRoute = ({ component: Component, ...rest }: any) => {
     return (
       // Show the component only when the user is logged in
       // Otherwise, redirect the user to /signin page
-      <Route
-        {...rest}
-        render={(props) =>
-          loggedIn ? <Component {...props} /> : <Redirect to="/Onboarding" />
-        }
-      />
+      <Route {...rest} render={(props) => <Component {...props} />} />
     );
 };
 
