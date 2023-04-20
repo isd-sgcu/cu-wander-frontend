268d465c4edbccfd5fe72078ac01a10995929336
samithiwat <samithiwat2544@gmail.com>
2023-04-21 06:01:50 +0700

    feat: create google map context and component

---
+++ src/components/step/GoogleMap.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useRef } from "react";
+
+const defaultOptions: google.maps.MapOptions = {
+  center: {
+    lat: 13.735342,
+    lng: 100.5318827,
+  },
+  zoom: 18,
+  fullscreenControl: false,
+  mapTypeControl: false,
+  streetViewControl: false,
+};
+
+export default () => {
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (!ref.current) return;
+    new window.google.maps.Map(ref.current, defaultOptions);
+  }, []);
+
+  return <div ref={ref} className="w-full" id="map" />;
+};
