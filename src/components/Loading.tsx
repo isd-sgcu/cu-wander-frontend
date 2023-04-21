ffa392497b852f641ea95323a6701032cf2da6e8
samithiwat <samithiwat2544@gmail.com>
2023-04-21 19:19:50 +0700

    feat: add logo in loading screen

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -1,4 +1,4 @@
-import { IonSpinner, SpinnerTypes } from "@ionic/react";
+import { IonImg, IonSpinner, IonText, SpinnerTypes } from "@ionic/react";
 
 type LoadingProps = {
   name: SpinnerTypes;
@@ -6,8 +6,16 @@ type LoadingProps = {
 
 export default ({ name }: LoadingProps) => {
   return (
-    <div className="flex items-center justify-center pt-20">
-      <IonSpinner name={name} class="text-green-500" />
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="w-full m-10 flex flex-col justify-center items-center gap-5">
+        <IonImg
+          src="assets/icon/shoe_green.svg"
+          className="w-1/3"
+          alt="loading"
+        />
+
+        <IonSpinner name={name} className="text-green-500" />
+      </div>
     </div>
   );
 };
